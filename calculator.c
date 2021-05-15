dcc50b60f8c0930758ef87033b5bc54a957f80d7
uvanshankarsj <84233610+uvanshankarsj@users.noreply.github.com>
2021-05-15 18:29:33 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -45,6 +45,18 @@ double squareroot(double a){
     return sqrt(a);
 }
 // Insert your code below
+#include<stdio.h>
+
+void ceiling(float x)
+{scanf("%f",x);
+int ceil;
+ceil=x/1;
+if(ceil<=0) 
+printf("the ceil of the number %d is %d",x,ceil);
+else
+ceil+=1;
+printf("the ceil of the number %d is %d",x,ceil);
+}
 
 int main() {
     add();
